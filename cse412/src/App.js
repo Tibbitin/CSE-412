dc22b3953dbaa626f229ab0a8d4724478884f3b9
Devam Chokshi <devamchokshi23@gmail.com>
2022-04-21 00:24:02 -0700

    ggg

--- cse412/src/App.js
+++ cse412/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import React, { useLayoutEffect, useState, } from "react";
 import 'bootstrap/dist/css/bootstrap.css'
-import Navibar from './components/navbarcomp/Navibar'
 import Signin from './pages/Signin/Signin';
 import Home from './pages/Store/Home'
 import Games from './pages/Games/Games'
 import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
-import {Nav, Navbar} from 'react-bootstrap'
-import logo from './assets/steamlogo.jpg';
 import Register from './pages/Register/Register'
 
 
